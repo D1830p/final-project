2e79fcecd107a10eeebe3000a71a080f175a754a
Dharti <dparikh54@gmail.com>
2019-12-13 22:45:34 -0500

    Added header

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import Info from './Components/Info';
 import Transactions from './Components/Transactions';
 import Allowance from './Components/Allowance';
 import Rewards from './Components/Rewards';
+import Header from './Components/Header';
 import './index';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App" id="root">
+      <div className="flex flex-col h-full" id="root">
+        <Header />
+
+        <div id="main" className="flex">
+          <div className="px-4 py-2 w-full md:w-1/2">
         <Info />
-        <br />
-        Transfer Balance:
+
         <Transactions />
-        <br />
-        Allowance:
+
         <Allowance />
-        <br />
-        Rewards:
+
         <Rewards />
-        <br />
+          </div>
+
+          <div id="hero" className="hidden md:block w-1/2">
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+  
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="flex px-4 justify-between w-screen bg-indigo-800 py-4">
+      <p className="text-white uppercase tracking-wide font-bold text-2xl">Painting Dapp</p>
+      <p className="text-white">Share Ownership of Antique Paintings</p>
+    </div>
+  );
+};
+
+export default Header;
